59a06fa45e7bb018d7c53956e1da89e2b1815dd1
ronohana <37269139+ronohana@users.noreply.github.com>
2018-03-28 19:55:51 +0300

    Adding POOPex1 files

---
+++ hw1/src/hw1/Customer.java
@@ -0,0 +1,22 @@
+package hw1;
+
+public class Customer {
+
+	
+	private String name;
+	
+
+	
+	public Customer(String nm) {
+		
+			this.name = nm;
+		
+	}
+	public void orderPC(String MB,String CPU,String Peripherals,String Screen) {
+		
+		
+		
+		
+	}
+
+}
---
+++ hw1/src/hw1/main.java
@@ -0,0 +1,20 @@
+package hw1;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//Telling the customer wich computer is needed for he's new computer!
+		
+		String Mb = new String ("Intel");
+		String CPU = new String("I7 Proccesor");
+		String Peripherals = new String ("Microsoft");
+		String	Screen = new String ("LG");
+		
+		Costumer Rin = new C
+		
+	}
+
+}
